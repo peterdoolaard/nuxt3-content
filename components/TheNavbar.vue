d4980d8ed110e11dbb8c40cdcd6f8cfc87103286
peterdoolaard <peter.doolaard@xs4all.nl>
2023-01-06 16:32:10 +0100

    Initial commit

---
+++ components/TheNavbar.vue
@@ -0,0 +1,17 @@
+<script setup>
+const isMobileNavOpen = ref(false)
+</script>
+
+<template>
+  <nav class="navbar">
+    <div class="container">
+      <div class="navbar-brand">
+        <NuxtLink to="/" class="navbar-item has-text-weight-bold is-size-5">
+          My Nuxt 3 Blog
+        </NuxtLink>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style></style>
---
+++ content/blog/my-second-blog-post.md
@@ -0,0 +1,13 @@
+---
+title: What is a counter?
+author: "@trudie"
+dates: 
+  published: "2023-01-06"
+description: Once upon a timer there was a counter
+---
+
+ ## What is a counter?
+
+It allows you to keep track of a number and increment it by clicking on a button.
+
+<Counter></Counter>
